0527d8f69cf98f7ec8f1607ad26ccd6ef401cfd4
ivanosidon <156651027+ivanosidon@users.noreply.github.com>
2025-03-31 12:03:52 +0200

    Add GitHub Pages deployment workflow and update site configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
 
 export default defineConfig({
+  site: 'https://ivan-de-jager248.github.io',
+  base: 'mics-website',
   integrations: [
     tailwind(),
     vue(),
