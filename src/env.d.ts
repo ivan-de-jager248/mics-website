55fd63daef3fff7f34ce78851369794e0c8acbf8
ivanosidon <156651027+ivanosidon@users.noreply.github.com>
2025-04-22 11:16:36 +0200

    Integrate subscription form as well and use environment variables instead

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly CONTACT_FORMSUBMIT_DEFAULT_ID: string;
+  readonly CONTACT_FORMSUBMIT_QUOTE_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
